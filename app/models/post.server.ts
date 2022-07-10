72720ff1cbec3bed4d70372d1dcef0ec3d72f58f
Misol Goh <melancholy8914@gmail.com>
2022-07-10 15:47:14 +0900

    Remix Server Code Filename Convention: Add post.server.ts to organise server data for posts

---
+++ app/models/post.server.ts
@@ -0,0 +1,11 @@
+export default async function getPosts() {
+    const posts = [{
+        slug: 'my-first-post',
+        title: 'My First Post!',
+    }, {
+        slug: 'trail-riding-with-onewheel',
+        title: 'Trail Riding with Onewheel',
+    }];
+
+    return posts;
+}
